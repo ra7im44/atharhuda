e199903907368bc1e774cf1f5241f4f4294f9565
Ahmed_Abouelwafa <hamada.abouelwafa12@gmail.com>
2026-02-14 11:08:12 +0200

    add

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  geminiApiKey: 'AIzaSyAy7uw8360gQjOzZ0CEkI4NRFjfLk1769k',
+  geminiProjectNumber: '982851632370',
+  geminiProjectName: 'projects/982851632370'
+};
